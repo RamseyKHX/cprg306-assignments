b69e15b88cfb924a8312f7d7c6fdac036b95d54d
RamseyKHX <rameez.ahmed@edu.sait.ca>
2024-09-09 03:36:07 -0600

    complete week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <h1>Rameez Ahmed</h1>
+
+            <ul>
+                <Link href="https://github.com/RamseyKHX/cprg306-assignments">GitHub repository </Link>
+            </ul>
+        </main>
+    );
+}
