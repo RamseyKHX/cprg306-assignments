1d5dbbd71dfba9a8b7a961d1a69e120c99ee761f
RamseyKHX <rameez.ahmed@edu.sait.ca>
2024-10-06 02:31:46 -0600

    Completed week 4
    Did some extra Tailwind styling.

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -13,26 +13,20 @@ export default function NewItem() {
   return(
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-center p-4">
-        <p className="text-xl font-bold mb-4">Count: {quantity}</p>
+        <p className="text-xl font-bold mb-4"> {quantity}</p>
         
         <div className="flex space-x-4">
           
           <button 
             onClick={increment} 
             className={`bg-blue-500 text-white w-10 rounded  ${quantity === 20 ? 'bg-gray-500' : ''}`}
-            disabled={quantity === 20}
-          >
-            +
-          </button>
+            disabled={quantity === 20}>+</button>
           
         
           <button 
             onClick={decrement} 
             className={`bg-blue-500 text-white w-10 rounded ${quantity === 1 ? 'bg-gray-500' : ''}`}
-            disabled={quantity === 1}
-          >
-            -
-          </button>
+            disabled={quantity === 1}>-</button>
         </div>
       </div>
     </div>
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -4,9 +4,11 @@ import NewItem from './new-item';
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center  bg-black-500">
-      <h1 className="text-2xl font-bold mb-4"></h1>
-      <NewItem />
+    <div className="flex flex-col items-center justify-center bg-black-100">
+      <div className="bg-red-800 p-8 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4 text-center"></h1>
+        <NewItem />
+      </div>
     </div>
   );
 }
